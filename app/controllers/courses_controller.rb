3e107b9987dc4e37bd95b5ef89a67978b09fcc05
Pi-Feng Lin <lin.pifeng@gmail.com>
2017-01-31 15:56:01 -0800

    initial commit

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   def index
-  	@search_term = params[:looking_for] || 'jhu'
+  	@search_term = params[:looking_for] || 'web'
   	@courses = Coursera.for(@search_term)
   end
 end
